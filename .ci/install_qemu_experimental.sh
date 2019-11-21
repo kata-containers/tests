af3e5a5d59923a411689d2c7c3f4e7a77bed7a31
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2019-11-21 23:52:32 +0000

    ci: fix install qemu-virtiofs
    Now the tarball provides a different name
    On untar be more verbose to see what got installed
    
    Fixes: #2110
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

--- .ci/install_qemu_experimental.sh
+++ .ci/install_qemu_experimental.sh
@@ -20,7 +20,7 @@ KATA_DEV_MODE="${KATA_DEV_MODE:-}"
 CURRENT_QEMU_TAG=$(get_version "assets.hypervisor.qemu-experimental.tag")
 QEMU_REPO_URL=$(get_version "assets.hypervisor.qemu-experimental.url")
 PACKAGING_REPO="github.com/kata-containers/packaging"
-QEMU_TAR="kata-qemu-static.tar.gz"
+QEMU_TAR="kata-static-qemu-virtiofsd.tar.gz"
 arch=$("${cidir}"/kata-arch.sh -d)
 INSTALL_LOCATION="/tmp/qemu-virtiofs-static/opt/kata/bin/"
 QEMU_PATH="/opt/kata/bin/qemu-virtiofs-system-x86_64"
@@ -30,7 +30,7 @@ qemu_experimental_latest_build_url="${jenkins_url}/job/qemu-experimental-$(uname
 uncompress_experimental_qemu() {
 	local qemu_tar_location="$1"
 	[ -n "$qemu_tar_location" ] || die "provide the location of the QEMU compressed file"
-	sudo tar -xf "${qemu_tar_location}" -C /
+	sudo tar -xvf "${qemu_tar_location}" -C /
 }
 
 install_cached_qemu_experimental() {
