6316c7e92904b03c78a0b7edc206004e7b9fd6bb
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2020-04-27 16:30:57 -0500

    ci: Do not install latest bats version
    We should not install the latest bats version as it has issues
    when we skip tests https://github.com/bats-core/bats-core/issues/292
    and when we are running in parallel https://github.com/bats-core/bats-core/issues/290.
    
    Fixes #2483
    
    Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>

--- .ci/install_bats.sh
+++ .ci/install_bats.sh
@@ -7,12 +7,16 @@
 
 set -e
 
-which bats && exit
-
+cidir=$(dirname "$0")
+source "${cidir}/lib.sh"
 BATS_REPO="github.com/bats-core/bats-core"
+version=$(get_test_version "externals.bats.version")
 
 echo "Install BATS from sources"
 go get -d "${BATS_REPO}" || true
 pushd "${GOPATH}/src/${BATS_REPO}"
+# This is related with https://github.com/bats-core/bats-core/issues/290
+# and with https://github.com/bats-core/bats-core/issues/292
+git checkout "${version}"
 sudo -E PATH=$PATH sh -c "./install.sh /usr"
 popd
