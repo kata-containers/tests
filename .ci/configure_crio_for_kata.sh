4fd3ac5cfaced43335859a4eb7a23d57f19c2bdf
Salvador Fuentes <salvador.fuentes@intel.com>
2020-05-04 18:08:23 -0500

    ci: update the way we get crio version
    We need to update how we get the value of the crio version
    as the output of `crio --version` has changed since
    crio 1.18.0.
    This change now works with new and old versions of crio.
    
    Fixes: #2506.
    
    Signed-off-by: Salvador Fuentes <salvador.fuentes@intel.com>

--- .ci/configure_crio_for_kata.sh
+++ .ci/configure_crio_for_kata.sh
@@ -21,7 +21,7 @@ crio_config_file="/etc/crio/crio.conf"
 runc_flag="\/usr\/local\/bin\/crio-runc"
 kata_flag="\/usr\/local\/bin\/kata-runtime"
 
-minor_crio_version=$(crio --version | head -1 | cut -d '.' -f2)
+minor_crio_version=$(crio --version | egrep -o "[0-9]+\.[0-9]+\.[0-9]+" | head -1 | cut -d '.' -f2)
 
 if [ "$minor_crio_version" -ge "12" ]; then
 	echo "Configure runtimes map for RuntimeClass feature"
