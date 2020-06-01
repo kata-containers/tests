a2d29eb1849bf1b7cba337bde76f9ce7471f17b8
Wainer dos Santos Moschetta <wainersm@redhat.com>
2020-06-01 13:00:33 -0400

    ci/install_kata: Allow to not run kata-check
    The .ci/install_kata.sh script runs the kata-check by the end of the
    installation process.
    
    This change makes the script look at the RUN_KATA_CHECK environment variable
    which can be set to "false" and then the checking is skipped.
    
    Fixes #2596
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- .ci/jenkins_job_build.sh
+++ .ci/jenkins_job_build.sh
@@ -36,6 +36,8 @@ init_ci_flags() {
 	# Use experimental kernel
 	# Values: true|false
 	export experimental_kernel="false"
+	# Run the kata-check checks
+	export RUN_KATA_CHECK="true"
 }
 
 source "/etc/os-release" || source "/usr/lib/os-release"
