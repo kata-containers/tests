11ea324de4144c5dc67e9e6fe9cb5514231089c9
bin liu <bin@hyper.sh>
2020-06-12 10:48:26 +0800

    ci: ignore license header check for drawio files
    This PR will ignore of chcking of drawio files.
    
    Fixes: #2637
    
    Signed-off-by: bin liu <bin@hyper.sh>

--- .ci/static-checks.sh
+++ .ci/static-checks.sh
@@ -445,6 +445,7 @@ static_check_license_headers()
 		--exclude="*.pub" \
 		--exclude="*.service" \
 		--exclude="*.svg" \
+		--exclude="*.drawio" \
 		--exclude="*.toml" \
 		--exclude="*.txt" \
 		--exclude="vendor/*" \
