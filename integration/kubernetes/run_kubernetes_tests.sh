cf935ad86d2608e110f2e033b12ec90e74feb8b3
bin liu <bin@hyper.sh>
2020-09-15 11:45:54 +0800

    k8s: add pod OOM test for containerd
    Now CRI-O not support OOM for Kata containers, first add
    tests for containerd.
    
    Fixes: #2853
    
    Signed-off-by: bin liu <bin@hyper.sh>

--- integration/kubernetes/run_kubernetes_tests.sh
+++ integration/kubernetes/run_kubernetes_tests.sh
@@ -43,6 +43,7 @@ K8S_TEST_UNION=("k8s-attach-handlers.bats" \
 	"k8s-liveness-probes.bats" \
 	"k8s-memory.bats" \
 	"k8s-number-cpus.bats" \
+	"k8s-oom.bats" \
 	"k8s-parallel.bats" \
 	"k8s-pid-ns.bats" \
 	"k8s-pod-quota.bats" \
