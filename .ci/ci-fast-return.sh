a4bb6f1c0d9557c79a810eaba66dd12aa9402d94
James O. D. Hunt <james.o.hunt@intel.com>
2020-10-01 09:31:17 +0100

    CI: Redirect jq install output to special fd
    Use the special file descriptor created for messages when installing
    `jq(1)` in `install_jq()` to avoid polluting the standard streams and
    causing the fast return script to fail.
    
    Also drops the `-E` from the `sudo(8)` call for SUSE for consistency
    with the other distro logic.
    
    Fixes: #2365.
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- .ci/ci-fast-return.sh
+++ .ci/ci-fast-return.sh
@@ -81,16 +81,16 @@ install_jq() {
 	package="jq"
 	case "$ID" in
 		centos|rhel)
-			sudo yum -y install "${package}"
+			sudo yum -y install "${package}" 1>&5 2>&1
 			;;
 		debian|ubuntu)
-			sudo apt-get -y install "${package}"
+			sudo apt-get -y install "${package}" 1>&5 2>&1
 			;;
 		opensuse-*|sles)
-			sudo -E zypper install -y "${package}"
+			sudo zypper install -y "${package}" 1>&5 2>&1
 			;;
 		fedora)
-			sudo dnf -y install "${package}"
+			sudo dnf -y install "${package}" 1>&5 2>&1
 			;;
 	esac
 }
