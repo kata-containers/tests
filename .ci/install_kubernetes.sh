5b83f51838f853e8c31b08a6569230547ce85bb3
Jing Wang <jing.wang4@ibm.com>
2021-01-28 21:54:32 +0000

    ci: updates to kata setup scripts for ppc64le
    - provide defaults and/or take user inputs(exports) of env parameters needed for kata setup on ppc64le.
    - kubenetes package installation for ppc64le
    - virtiofsd installation
    
    Fixes: #3185
    Signed-off-by: Jing Wang <jing.wang4@ibm.com>

--- .ci/install_kubernetes.sh
+++ .ci/install_kubernetes.sh
@@ -18,6 +18,7 @@ echo "Install Kubernetes components"
 cidir=$(dirname "$0")
 source /etc/os-release || source /usr/lib/os-release
 kubernetes_version=$(get_version "externals.kubernetes.version")
+ARCH=$(uname -m)
 
 if [ "$ID" == "ubuntu" ] || [ "$ID" == "debian" ]; then
 	sudo bash -c "cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
@@ -29,10 +30,15 @@ EOF"
 	chronic sudo -E apt update
 	chronic sudo -E apt install --allow-downgrades -y kubelet="$kubernetes_version" kubeadm="$kubernetes_version" kubectl="$kubernetes_version"
 elif [ "$ID" == "centos" ] || [ "$ID" == "fedora" ]; then
-	sudo bash -c "cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+	url=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+        if [ "${ARCH}" == "ppc64le" ]; then
+           url=https://packages.cloud.google.com/yum/repos/kubernetes-el7-ppc64le
+        fi
+        echo "Install ${url} for ${ARCH}"
+        sudo bash -c "cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 	[kubernetes]
 	name=Kubernetes
-	baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+        baseurl=${url}
 	enabled=1
 	gpgcheck=1
 	repo_gpgcheck=1
