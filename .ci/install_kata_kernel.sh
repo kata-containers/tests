efe0797eb617015116490bbea7215b3e82548384
Wainer dos Santos Moschetta <wainersm@redhat.com>
2021-05-04 14:49:40 -0400

    ci/install_kata_kernel: Use cached experimental kernel
    Likewise the script was changed to get the experimental kernel from
    http://jenkins.katacontainers.io/job/kata-containers-2.0-kernel-experimental-x86_64-nightly
    
    Fixes  #3119
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>
    (cherry picked from commit 0d51cd08ffaa67dbbb5605bedd47465c8a91b76e)
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>

--- .ci/install_kata_kernel.sh
+++ .ci/install_kata_kernel.sh
@@ -17,7 +17,7 @@ source "${cidir}/lib.sh"
 source "/etc/os-release" || source "/usr/lib/os-release"
 
 latest_build_url="${jenkins_url}/job/kata-containers-2.0-kernel-vanilla-$(uname -m)-nightly/${cached_artifacts_path}"
-experimental_latest_build_url="${jenkins_url}/job/kernel-experimental-nightly-$(uname -m)/${cached_artifacts_path}"
+experimental_latest_build_url="${jenkins_url}/job/kata-containers-2.0-kernel-experimental-$(uname -m)-nightly/${cached_artifacts_path}"
 PREFIX="${PREFIX:-/usr}"
 kernel_dir="${DESTDIR:-}${PREFIX}/share/kata-containers"
 
@@ -39,11 +39,10 @@ trap exit_handler EXIT
 get_current_kernel_version() {
 	if [ "$experimental_kernel" == "true" ]; then
 		kernel_version=$(get_version "assets.kernel-experimental.tag")
-		echo "${kernel_version}"
 	else
 		kernel_version=$(get_version "assets.kernel.version")
-		echo "${kernel_version/v/}"
 	fi
+	echo "${kernel_version/v/}"
 }
 
 get_kata_config_version() {
@@ -130,6 +129,7 @@ main() {
 	if [ "${experimental_kernel}" == "false" ]; then
 		cached_kernel_version=$(curl -sfL "${latest_build_url}/latest") || cached_kernel_version="none"
 	else
+		current_kernel_version+="-experimental"
 		cached_kernel_version=$(curl -sfL "${experimental_latest_build_url}/latest") || cached_kernel_version="none"
 	fi
 	info "current kernel : ${current_kernel_version}"
