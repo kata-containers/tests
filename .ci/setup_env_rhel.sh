2325c2a2cfb61efa7ccc80edfc38f7774c0e4c0c
Jakob Naucke <jakob.naucke@ibm.com>
2021-06-09 18:38:13 +0200

    ci: Fix packages in RHEL env setup
    - Install make
    - Install ninja-build for QEMU
    - Install libpmem-devel only on x86_64
    - Install protobuf-compiler on ppc64le & s390x for agent
    - Install openssl-devel on s390x for kernel
    
    Signed-off-by: Jakob Naucke <jakob.naucke@ibm.com>

--- .ci/setup_env_rhel.sh
+++ .ci/setup_env_rhel.sh
@@ -28,8 +28,8 @@ declare -A minimal_packages=( \
 )
 
 declare -A packages=(
-	[kata_containers_dependencies]="libtool libtool-ltdl-devel device-mapper-persistent-data lvm2 device-mapper-devel libtool-ltdl bzip2 m4 patch gettext-devel automake autoconf bc pixman-devel coreutils" \
-	[qemu_dependencies]="libcap-devel libcap-ng-devel libattr-devel libcap-ng-devel librbd1-devel flex libfdt-devel libpmem-devel" \
+	[kata_containers_dependencies]="libtool libtool-ltdl-devel device-mapper-persistent-data lvm2 device-mapper-devel libtool-ltdl bzip2 m4 patch gettext-devel automake autoconf bc pixman-devel coreutils make" \
+	[qemu_dependencies]="libcap-devel libcap-ng-devel libattr-devel libcap-ng-devel librbd1-devel flex libfdt-devel ninja-build" \
 	[kernel_dependencies]="elfutils-libelf-devel flex" \
 	[crio_dependencies]="glibc-static libseccomp-devel libassuan-devel libgpg-error-devel device-mapper-libs btrfs-progs-devel util-linux gpgme-devel glib2-devel glibc-devel libselinux-devel pkgconfig" \
 	[bison_binary]="bison" \
@@ -44,6 +44,18 @@ declare -A packages=(
         [libsystemd]="systemd-devel" \
 	[redis]="redis" \
 )
+
+if [ "$(uname -m)" == "x86_64" ] ; then
+	packages[qemu_dependencies]+=" libpmem-devel"
+fi
+
+if [ "$(uname -m)" == "ppc64le" ] || [ "$(uname -m)" == "s390x" ]; then
+	packages[kata_containers_dependencies]+=" protobuf-compiler"
+fi
+
+if [ "$(uname -m)" == "s390x" ]; then
+	packages[kernel_dependencies]+=" openssl-devel"
+fi
 
 main()
 {
