cc0e398766e0ab6cf3edafd95e7a026c96eba75b
Julio Montes <julio.montes@intel.com>
2021-06-24 16:07:42 -0500

    integration/pmem: reimplement pmem test
    Use kubernetes + pmem-csi plugin to check pmem support
    in kata containers.
    This new implementation is intended to run on a bare metal
    with real pmem (i.e. Intel Optane) devices.
    
    fixes #3223
    
    Signed-off-by: Julio Montes <julio.montes@intel.com>

--- integration/pmem/data/lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Copyright (c) 2021 Intel Corporation
-#
-# SPDX-License-Identifier: Apache-2.0
-
-db_name="test"
-db_table_name="users"
--- integration/pmem/pmem_test.sh
+++ integration/pmem/pmem_test.sh
@@ -15,15 +15,8 @@ source "${dir_path}/../../lib/common.bash"
 source "${dir_path}/../../.ci/lib.sh"
 source "${dir_path}/data/lib.sh"
 source /etc/os-release || source /usr/lib/os-release
-osbuilder_repository="github.com/kata-containers/osbuilder"
-osbuilder_repository_path="${GOPATH}/src/${osbuilder_repository}"
-test_directory_name="test_pmem1"
-test_directory=$(mktemp -d --suffix="${test_directory_name}")
 TEST_INITRD="${TEST_INITRD:-no}"
 experimental_qemu="${experimental_qemu:-false}"
-# List of containers to remove before exit
-list_containers=()
-RUNTIME="io.containerd.kata.v2"
 arch=$("${dir_path}"/../../.ci/kata-arch.sh -d)
 
 if [ "$TEST_INITRD" == "yes" ]; then
@@ -41,125 +34,94 @@ if [ "$arch" == "aarch64" ]; then
 	exit 0
 fi
 
-function setup() {
-	sudo systemctl restart containerd
-	clean_env_ctr
-	check_processes
-	if [ ! -d "${osbuilder_repository_path}" ]; then
-		go get -d "${osbuilder_repository}" || true
-	fi
+if [ "${ID}" != "ubuntu" ]; then
+	echo "Skip pmem test: ${ID} distro is not supported"
+	exit 0
+fi
+
+init() {
+	${dir_path}/../../integration/kubernetes/init.sh
+	docker run -d -p 5000:5000 --restart=always --name registry registry:2
 }
 
-function ctr_rm_rf {
-	container=$1
-	while [ -n "$(ctr t list | grep RUNNING | grep "${container}")" ]; do
-		ctr t kill -a "${container}"
-		sleep 1
-	done
-	ctr c rm "${container}"
+cleanup() {
+	set +e
+	kubectl describe pod/my-csi-kata-app
+	kubectl get pod --all-namespaces
+
+	pushd pmem-csi
+	kubectl delete -f "deploy/common/pmem-kata-app.yaml"
+	kubectl delete -f "deploy/common/pmem-kata-pvc.yaml"
+	kubectl delete -f "deploy/common/pmem-storageclass-ext4-kata.yaml"
+	kubectl delete PmemCSIDeployment/pmem-csi.intel.com
+	kubectl delete -f "deploy/crd/pmem-csi.intel.com_pmemcsideployments.yaml"
+	kubectl delete -f "deploy/operator/pmem-csi-operator.yaml"
+	popd
+	rm -rf pmem-csi
+
+	${dir_path}/../../integration/kubernetes/cleanup_env.sh
+
+	docker rm -f $(docker ps -aq)
+	rid=$(docker images -q registry:2)
+	docker rmi -f $(docker images -q | sed '/'$rid'/d')
 }
 
-function test_pmem_mount {
-	local container_name="test-pmem"
-	local payload="tail -f /dev/null"
-	local image="docker.io/library/fedora:latest"
-
-	"${dir_path}/../../cmd/pmemctl/pmemctl.sh" -s 128M -f xfs -m "${test_directory}" xfs.img
-
-	ctr image pull "${image}"
-
-	list_containers+=(${container_name})
-
-	# Running container
-	ctr run -d --runtime ${RUNTIME} --mount type=bind,src="${test_directory}",dst="/${test_directory_name}",options=rbind:rw "${image}" "${container_name}" sh -c "${payload}"
-
-	# Check container
-	ctr t exec --exec-id 1 "${container_name}" mount | grep ${test_directory_name} | grep '/dev/pmem' | grep 'dax'
-
-	ctr_rm_rf "${container_name}"
-	sudo umount "${test_directory}"
-	sudo losetup -D
-	rm -f xfs.img
+run_test() {
+	kubectl exec pod/my-csi-kata-app -- df /data | grep pmem
+	kubectl exec pod/my-csi-kata-app -- mount | grep pmem | grep data | grep dax
 }
 
-function wait_for_postgres {
-	container="$1"
-	for i in $(seq 1 20); do
-		if ctr t exec --exec-id 1 "${container}" su - postgres -c "psql -c '\l'"; then
-			break
-		fi
-		sleep 5
-	done
+setup_pmem_csi() {
+	git clone https://github.com/intel/pmem-csi/
+	pushd pmem-csi
+
+	make build-images
+	make push-images
+
+	kubectl label node $(hostname) storage=pmem
+	kubectl label node $(hostname) katacontainers.io/kata-runtime=true
+
+	oper_yml="deploy/operator/pmem-csi-operator.yaml"
+	sed -i -e 's|image:.*|image: localhost:5000/pmem-csi-driver:canary|g' \
+		-e 's|imagePullPolicy:.*|imagePullPolicy: Always|g' \
+		${oper_yml}
+	kubectl apply -f "${oper_yml}"
+
+	kubectl apply -f deploy/crd/pmem-csi.intel.com_pmemcsideployments.yaml
+
+	kubectl create -f - <<EOF
+apiVersion: pmem-csi.intel.com/v1beta1
+kind: PmemCSIDeployment
+metadata:
+  name: pmem-csi.intel.com
+spec:
+  deviceMode: direct
+  nodeSelector:
+    storage: pmem
+EOF
+
+	stclass_yml="deploy/common/pmem-storageclass-ext4-kata.yaml"
+	kubectl apply -f "${stclass_yml}"
+
+	pvc_yml="deploy/common/pmem-kata-pvc.yaml"
+	kubectl apply -f "${pvc_yml}"
+
+	app_yml="deploy/common/pmem-kata-app.yaml"
+	sed -i -e 's|io.katacontainers.config.hypervisor.memory_offset:.*|io.katacontainers.config.hypervisor.memory_offset: "4294967296"|g' \
+		-e 's|runtimeClassName:.*|runtimeClassName: kata|g' \
+		"${app_yml}"
+	kubectl apply -f "${app_yml}"
+	popd
+
+	kubectl wait --for=condition=Ready --timeout=90s pod/my-csi-kata-app
+	sleep 5
 }
 
-function test_database {
-	"${dir_path}/../../cmd/pmemctl/pmemctl.sh" -s 1G -f xfs -m "${test_directory}" xfs.img
-
-	rows=10000
-	cont_image="docker.io/library/postgres:latest"
-	postgresql_cont_dir="/var/lib/postgresql/data"
-
-	ctr image pull "${cont_image}"
-
-	producer_cont="producer"
-	list_containers+=(${producer_cont})
-	ctr run --runtime ${RUNTIME} -d \
-		--mount type=bind,src="${test_directory}",dst="${postgresql_cont_dir}",options=rbind:rw \
-		--mount type=bind,src=$(realpath ${dir_path}/data),dst=/data,options=rbind:rw \
-		--env POSTGRES_PASSWORD=mysecretpassword --env PGDATA="${postgresql_cont_dir}/pgdata" ${cont_image} "${producer_cont}"
-	ctr t exec --exec-id 1 "${producer_cont}" sh -c "mount | grep ${postgresql_cont_dir} | grep dax"
-	ctr t exec --exec-id 1 "${producer_cont}" chown postgres "${postgresql_cont_dir}"
-	wait_for_postgres "${producer_cont}"
-
-	echo "Inserting into the database..."
-	ctr t exec --exec-id 1 "${producer_cont}" su - postgres -c "/data/dbinsert.sh ${rows}"
-	ctr_rm_rf "${producer_cont}"
-
-	consumer_cont="consumer"
-	list_containers+=(${consumer_cont})
-	ctr run --runtime ${RUNTIME} -d \
-		--mount type=bind,src="${test_directory}",dst="${postgresql_cont_dir}/",options=rbind:rw \
-		--env POSTGRES_PASSWORD=mysecretpassword --env PGDATA="${postgresql_cont_dir}/pgdata" ${cont_image} "${consumer_cont}"
-	wait_for_postgres "${consumer_cont}"
-
-	echo "Checking the data base..."
-	ctr t exec --exec-id 1 "${consumer_cont}" su - postgres -c "psql -d \"${db_name}\" \
-		-c \"select count(*) from ${db_table_name}\" | grep ${rows}"
-
-	[ -n "$(ctr t exec --exec-id 1 "${consumer_cont}" su - postgres -c "psql -d \"${db_name}\" \
-	  --tuples-only -c \"select name from ${db_table_name} where id=1;\"")" ]
-
-	[ -n "$(ctr t exec --exec-id 1 "${consumer_cont}" su - postgres -c "psql -d \"${db_name}\" \
-	  --tuples-only -c \"select name from ${db_table_name} where id=${rows+100};\"")" ]
-
-	# check random rows
-	for i in $(seq 1 20); do
-		[ -n "$(ctr t exec --exec-id 1 "${consumer_cont}" su - postgres -c "psql -d \"${db_name}\" \
-		  --tuples-only -c \"select name from ${db_table_name} where id=$((RANDOM%rows+1));\"")" ]
-	done
-
-	ctr_rm_rf "${consumer_cont}"
+main() {
+	trap cleanup EXIT QUIT KILL
+	init
+	setup_pmem_csi
+	run_test
 }
 
-function teardown() {
-	sudo umount "${test_directory}"
-	sudo losetup -D
-	sudo rm -rf "${test_directory}"
-	for c in ${list_containers[*]}; do
-		ctr_rm_rf "${c}" 2&> /dev/null || true
-	done
-
-	clean_env_ctr
-	check_processes
-}
-
-trap teardown EXIT
-
-echo "Running setup"
-setup
-
-echo "Running pmem mount test"
-test_pmem_mount
-
-echo "Running database pmem test"
-test_database
+main
