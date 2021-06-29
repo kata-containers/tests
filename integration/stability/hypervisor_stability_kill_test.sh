f2c00fe8312338dc9bff2ec35709a7ca3adae2d8
Wainer dos Santos Moschetta <wainersm@redhat.com>
2021-06-29 10:19:41 -0400

    integration/stability: Ignore running hypervisor processes
    Currently the statility test script is going to fail if there is any hypervisor
    process running on the system, but this situation can happen quit often, for
    instance, in case of leftovers from previously executed tests.
    
    Instead of failing the test, let's ignore the PIDs of any already running hypervisor
    process.
    
    Fixes #3654
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- integration/stability/hypervisor_stability_kill_test.sh
+++ integration/stability/hypervisor_stability_kill_test.sh
@@ -17,6 +17,9 @@ source "${cidir}/../../metrics/lib/common.bash"
 IMAGE="${IMAGE:-quay.io/prometheus/busybox:latest}"
 CONTAINER_NAME="${CONTAINER_NAME:-test}"
 PAYLOAD_ARGS="${PAYLOAD_ARGS:-tail -f /dev/null}"
+# The based pidof command-line. This should have appended the ID of processes
+# which should be ignored, so use this variable instead of plain `pidof`.
+pidof_cmd="pidof"
 
 setup()  {
 	restart_containerd_service
@@ -25,18 +28,23 @@ setup()  {
 	sudo ctr image pull $IMAGE
 	[ $? != 0 ] && die "Unable to get image $IMAGE"
 
+	# Build the base pidof command-line so that PID of running hypervisors
+	# are omitted.
+	for omit_pid in $(pidof ${HYPERVISOR_NAME}); do
+		pidof_cmd+=" -o $omit_pid"
+	done
 	sudo ctr run --runtime=$CTR_RUNTIME -d $IMAGE $CONTAINER_NAME sh -c $PAYLOAD_ARGS
-	num=$(pidof ${HYPERVISOR_NAME} | wc -w)
+	num=$(${pidof_cmd} ${HYPERVISOR_NAME} | wc -w)
 	[ ${num} -eq 1 ] || die "hypervisor count:${num} expected:1"
 }
 
 kill_hypervisor()  {
-	pid=$(pidof ${HYPERVISOR_NAME})
+	pid=$(${pidof_cmd} ${HYPERVISOR_NAME})
 	[ -n ${pid} ] || die "failed to find hypervisor pid"
 	sudo kill -KILL ${pid} || die "failed to kill hypervisor (pid ${pid})"
 	# signal is async and we've seen failures hypervisor not being killed immediately.
 	sleep 1
-	num=$(pidof ${HYPERVISOR_NAME} | wc -w)
+	num=$(${pidof_cmd} ${HYPERVISOR_NAME} | wc -w)
 	[ ${num} -eq 0 ] || die "hypervisor count:${num} expected:0"
 	sudo ctr tasks rm -f $(sudo ctr task list -q)
 	sudo ctr c rm $(sudo ctr c list -q)
