758edc34337ae678bd1a30f0c6d5d03261cb3dc2
Julio Montes <julio.montes@intel.com>
2021-07-05 16:07:28 -0500

    ci: pmem: containerd: allow image pulling from localhost registry
    pmem test requires image pulling from localhost:5000.
    Configures containerd correctly to allow image pulling from a local
    registry.
    
    fixes #3680
    
    Signed-off-by: Julio Montes <julio.montes@intel.com>

--- .ci/configure_containerd_for_kata.sh
+++ .ci/configure_containerd_for_kata.sh
@@ -29,4 +29,6 @@ cat << EOT | sudo tee /etc/containerd/config.toml
         [plugins.cri.containerd.runtimes.kata]
            runtime_type = "io.containerd.kata.v2"
            privileged_without_host_devices = true
+    [plugins.cri.registry.mirrors."localhost:5000"]
+      endpoint = ["http://localhost:5000"]
 EOT
