ca4c809f0f705a7641147a50678c5883f3e0c140
Manabu Sugimoto <Manabu.Sugimoto@sony.com>
2021-07-16 17:51:55 +0900

    ci: Add C++ compiler to build gperf
    This adds C++ compiler (`gcc-c++`) to centos and fedora in order to
    build gperf.
    The kata-agent will support seccomp, so libseccomp library is required
    and the gperf needed to build the library requires C++ compiler.
    
    Fixes: #3669
    
    Signed-off-by: Manabu Sugimoto <Manabu.Sugimoto@sony.com>

--- .ci/setup_env_centos.sh
+++ .ci/setup_env_centos.sh
@@ -63,6 +63,7 @@ declare -A packages=( \
 	[qemu_dependencies]="libcap-devel libcap-ng-devel libattr-devel libcap-ng-devel librbd1-devel flex libfdt-devel libpmem-devel ninja-build" \
 	[kernel_dependencies]="elfutils-libelf-devel flex pkgconfig patch" \
 	[crio_dependencies]="glibc-static libseccomp-devel libassuan-devel libgpg-error-devel util-linux libselinux-devel" \
+	[gperf_dependencies]="gcc-c++" \
 	[bison_binary]="bison" \
 	[libgudev1-dev]="libgudev1-devel" \
 	[general_dependencies]="gpgme-devel glib2-devel glibc-devel bzip2 m4 gettext-devel automake autoconf pixman-devel coreutils" \
