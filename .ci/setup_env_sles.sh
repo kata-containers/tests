a5b54d49fb1827ed762f36a6fd1f40d2aca39895
Jakob Naucke <jakob.naucke@ibm.com>
2021-07-19 17:14:02 +0200

    ci: Fix SLE package install
    - Skip all the individual repositories, just add openSUSE Leap OSS.
      On s390x, use 15.3 or newer, because that is the earliest openSUSE
      that is built.
    - Allow pandoc to not be installed (e.g. currently not in openSUSE Leap
      15.3 OSS s390x)
    - Install libpmem on supported architectures only (cf. #2341)
    - Install ninja for QEMU (cf. #3264)
    - Install protobuf for required architectures (cf. #3612)
    
    Fixes: #3728
    Signed-off-by: Jakob Naucke <jakob.naucke@ibm.com>

--- .ci/setup_env_sles.sh
+++ .ci/setup_env_sles.sh
@@ -12,33 +12,26 @@ source "/etc/os-release" || source "/usr/lib/os-release"
 source "${cidir}/lib.sh"
 arch=$("${cidir}"/kata-arch.sh -d)
 
-echo "Add repo for perl-IPC-Run"
-perl_repo="https://download.opensuse.org/repositories/devel:languages:perl/SLE_${VERSION//-/_}/devel:languages:perl.repo"
-sudo -E zypper addrepo --no-gpgcheck ${perl_repo}
-sudo -E zypper refresh
+# Use at least openSUSE 15.3 on s390x because that is the first release
+major="${VERSION_ID%.*}"
+minor="${VERSION_ID#*.}"
+[ "${arch}" == "s390x" ] && ([[ "${major}" -le 12 ]] || ([ "${major}" == "15" ] && [ "${minor}" -le 2 ])) && \
+	VERSION_ID="15.3"
 
-echo "Add repo for myspell"
-leap_repo="http://download.opensuse.org/update/leap/15.0/oss/"
+leap_repo="http://download.opensuse.org/distribution/leap/${VERSION_ID}/repo/oss/"
 leap_repo_name="leap-oss"
+sudo -E zypper removerepo ${leap_repo} || true
 sudo -E zypper addrepo --no-gpgcheck ${leap_repo} ${leap_repo_name}
 sudo -E zypper refresh  ${leap_repo_name}
 
 echo "Install perl-IPC-Run"
 sudo -E zypper -n install perl-IPC-Run
 
-echo "Add repo for moreutils"
-moreutils_repo="https://download.opensuse.org/repositories/utilities/SLE_${VERSION//-/_}/utilities.repo"
-sudo -E zypper addrepo --no-gpgcheck ${moreutils_repo}
-sudo -E zypper refresh
-
-echo "Add repo for hunspell and pandoc packages"
-sudo -E SUSEConnect -p PackageHub/${VERSION_ID}/${arch}
-
 echo "Install chronic"
 sudo -E zypper -n install moreutils
 
 declare -A minimal_packages=( \
-	[spell-check]="hunspell myspell-en_GB myspell-en_US pandoc" \
+	[spell-check]="hunspell myspell-en_GB myspell-en_US" \
 	[xml_validator]="libxml2-tools" \
 	[yaml_validator_dependencies]="python-setuptools" \
 )
@@ -46,7 +39,7 @@ declare -A minimal_packages=( \
 declare -A packages=( \
 	[general_dependencies]="curl git patch"
 	[kata_containers_dependencies]="libtool automake autoconf bc libpixman-1-0-devel coreutils" \
-	[qemu_dependencies]="libcap-devel libattr1 libcap-ng-devel librbd-devel libpmem-devel" \
+	[qemu_dependencies]="libcap-devel libattr1 libcap-ng-devel librbd-devel ninja" \
 	[kernel_dependencies]="libelf-devel flex" \
 	[crio_dependencies]="libglib-2_0-0 libseccomp-devel libapparmor-devel libgpg-error-devel glibc-devel-static libgpgme-devel libassuan-devel glib2-devel glibc-devel util-linux" \
 	[bison_binary]="bison" \
@@ -57,6 +50,18 @@ declare -A packages=( \
 	[haveged]="haveged" \
 	[libsystemd]="systemd-devel" \
 )
+
+if [ "${arch}" == "x86_64" ] || [ "${arch}" == "ppc64le" ]; then
+	packages[qemu_dependencies]+=" libpmem-devel"
+fi
+
+if [ "$(uname -m)" == "ppc64le" ] || [ "$(uname -m)" == "s390x" ]; then
+	packages[kata_containers_dependencies]+=" protobuf-devel"
+fi
+
+if [ "$(uname -m)" == "s390x" ]; then
+	packages[kernel_dependencies]+=" libopenssl-devel"
+fi
 
 main()
 {
@@ -80,23 +85,21 @@ main()
 
 	chronic sudo -E zypper -n install $pkgs_to_install
 
+	# Pandoc currently unavailable in openSUSE s390x repos
+	# Allow install failure, is not required for mainstream CI workflow
+	echo "Try to install pandoc"
+	sudo -E zypper -n install pandoc || true
+
 	echo "Install YAML validator"
 	chronic sudo -E easy_install pip
 	chronic sudo -E pip install yamllint
 
-	echo "Add redis repo and install redis"
-	redis_repo="https://download.opensuse.org/repositories/server:database/SLE_${VERSION//-/_}/server:database.repo"
-	chronic sudo -E zypper addrepo --no-gpgcheck ${redis_repo}
-	chronic sudo -E zypper refresh
+	echo "Install redis"
 	chronic sudo -E zypper -n install redis
 
 	[ "$setup_type" = "minimal" ] && exit 0
 
-	echo "Add crudini repo"
-	VERSIONID="12_SP1"
-	crudini_repo="https://download.opensuse.org/repositories/Cloud:OpenStack:Liberty/SLE_${VERSIONID}/Cloud:OpenStack:Liberty.repo"
-	chronic sudo -E zypper addrepo --no-gpgcheck ${crudini_repo}
-	chronic sudo -E zypper refresh
+	echo "Install crudini"
 	chronic sudo -E zypper -n install crudini
 }
 
