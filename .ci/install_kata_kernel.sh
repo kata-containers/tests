a4b917882262feb901f34896a53ca84957ac9ce4
David Gibson <david@gibson.dropbear.id.au>
2021-07-27 14:11:57 +1000

    tests/install_kata_kernel: Simplify fallback logic
    The script currently checks the kernel version available in the latest
    cached build and compares that against the desired version.  It only
    attempts to download the cached kernel if the versions match.
    
    But, the URLs for the cached kernels already include the version.  We don't
    actually care what the cached version is - only whether the one we want has
    a prebuilt image.  So, always attempt to download the cached kernel and
    fallback to building from source otherwise.  This simplifies and shortens
    the script a bit.
    
    While we're there update some of the informational messages; remove some
    that don't really give useful information and update others to better
    reflect the new logic.
    
    fixes #3758
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- .ci/install_kata_kernel.sh
+++ .ci/install_kata_kernel.sh
@@ -42,30 +42,28 @@ build_and_install_kernel() {
 install_cached_kernel(){
 	local kernel_binary=${1:-}
 	[ -z "${kernel_binary}" ] && die "empty binary format"
-	info "Installing ${kernel_binary}"
+	info "Attempting to download cached ${kernel_binary}"
 	sudo mkdir -p "${kernel_dir}"
 	local kernel_binary_name="${kernel_binary}-${cached_kernel_version}"
 	local kernel_binary_path="${kernel_dir}/${kernel_binary_name}"
 	curl -fL --progress-bar "${latest_build_url}/${kernel_binary_name}" -o "${tmp_dir}/${kernel_binary_name}" || return 1
 	sudo mv ${tmp_dir}/${kernel_binary_name} ${kernel_binary_path}
 	kernel_symlink="${kernel_dir}/${kernel_binary}.container"
-	info "Installing ${kernel_binary_path} and symlink ${kernel_symlink}"
 	sudo -E ln -sf "${kernel_binary_path}" "${kernel_symlink}"
 }
 
 install_prebuilt_kernel() {
-	info "Install pre-built kernel version"
-
 	for k in "vmlinux" "vmlinuz"; do
 		install_cached_kernel "${k}" || return 1
 	done
 
 	pushd "${kernel_dir}" >/dev/null
-	info "Verify download checksum"
 	curl -fsL "${latest_build_url}/sha256sum-kernel" -o ${tmp_dir}/sha256sum-kernel || return 1
 	sudo mv ${tmp_dir}/sha256sum-kernel .
 	sudo sha256sum -c "sha256sum-kernel" || return 1
 	popd >/dev/null
+
+	info "Installed pre-built kernel"
 }
 
 main() {
@@ -73,19 +71,14 @@ main() {
 	kernel_version=$(get_version "assets.kernel.version")
 	kernel_version=${kernel_version#v}
 	kata_config_version=$(cat "${kernel_packaging_dir}/kata_config_version")
-	current_kernel_version="${kernel_version}-${kata_config_version}"
+	kata_kernel_version="${kernel_version}-${kata_config_version}"
 	cached_kernel_version=$(curl -sfL "${latest_build_url}/latest") || cached_kernel_version="none"
-	info "current kernel : ${current_kernel_version}"
+	info "Kata guest kernel : ${kata_kernel_version}"
 	info "cached kernel  : ${cached_kernel_version}"
-	if [ "$cached_kernel_version" == "$current_kernel_version" ] && [ "$(arch)" == "x86_64" ]; then
-		# If installing kernel fails,
-		# then build and install it from sources.
-		if ! install_prebuilt_kernel; then
-			info "failed to install cached kernel, trying to build from source"
-			build_and_install_kernel
-		fi
-	else
-		build_and_install_kernel
+
+	if ! install_prebuilt_kernel; then
+	    info "failed to install cached kernel, trying to build from source"
+	    build_and_install_kernel
 	fi
 }
 
