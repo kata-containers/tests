cf54c4788d39cc7808527f2e91ac52a776cac003
stevenhorsman <steven@uk.ibm.com>
2021-08-26 17:16:38 +0100

    CCv0 | ci: Add CCv0 features into CI script
    Make the build understand if they are running against a target branch
    of CCv0 or main and update branches dynamically based on this
    
    Add code to get containerd from our CCv0 PR rather than a released build
    
    Add new CCv0 condition to the code that created the kata image.
    CCv0 requires some extra packages for running and debug. It also needs
    umoci, which is not yet availabe as a distro package, so wget it direct
    
    Fixes #3887
    
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- .ci/install_kata_image.sh
+++ .ci/install_kata_image.sh
@@ -22,6 +22,7 @@ initrd_name="${initrd_name:-kata-containers-initrd.img}"
 AGENT_INIT="${AGENT_INIT:-${TEST_INITRD:-no}}"
 TEST_INITRD="${TEST_INITRD:-no}"
 build_method="${BUILD_METHOD:-distro}"
+EXTRA_PKGS="${EXTRA_PKGS:-}"
 
 build_rust_image() {
 	export RUST_AGENT="yes"
@@ -42,13 +43,30 @@ build_rust_image() {
 	info "Building ${target_image} with AGENT_INIT=${AGENT_INIT}"
 	case "$build_method" in
 		"distro")
-			distro="${osbuilder_distro:-ubuntu}"
-			if [[ ! "${osbuild_docker:-}" =~ ^(0|false|no)$ ]]; then
-				use_docker="${osbuild_docker:-}"
-				[[ -z "${USE_PODMAN:-}" ]] && use_docker="${use_docker:-1}"
+			if [ ${CCV0} == "yes"]; then
+				# CCv0 is using skopeo and gpg packages and we've added a few more for debugging. The default distro of ubuntu is really back level, so some are missing there, so use fedora
+				# We also need to split the rootfs create and image build so we can add umoci to the rootfs directly
+				distro=fedora
+				EXTRA_PKGS="skopeo gnupg gpgme-devel vim iputils net-tools iproute"
+				sudo -E USE_DOCKER="${use_docker:-}" DISTRO="${distro}" EXTRA_PKGS="${EXTRA_PKGS}" \
+					make -e "rootfs"
+				rootfs_target="$(shell pwd)/$(DISTRO)_rootfs"
+				# CCv0 is using umoci which isn't in a fedora package we can access yet, so grabbing from their webstire
+				go_arch=$("${cidir}"/kata-arch.sh -g)
+				mkdir -p ${rootfs_target}/usr/local/bin/
+				sudo curl -Lo ${rootfs_target}/usr/local/bin/umoci https://github.com/opencontainers/umoci/releases/download/v0.4.7/umoci.${go_arch}
+				sudo chmod u+x ${rootfs_target}/usr/local/bin/umoci
+				sudo -E USE_DOCKER="${use_docker:-}" DISTRO="${distro}" \
+					make -e "image"
+			else
+				distro="${osbuilder_distro:-ubuntu}"
+				if [[ ! "${osbuild_docker:-}" =~ ^(0|false|no)$ ]]; then
+					use_docker="${osbuild_docker:-}"
+					[[ -z "${USE_PODMAN:-}" ]] && use_docker="${use_docker:-1}"
+				fi
+				sudo -E USE_DOCKER="${use_docker:-}" DISTRO="${distro}" EXTRA_PKGS="${EXTRA_PKGS}" \
+					make -e "${target_image}"
 			fi
-			sudo -E USE_DOCKER="${use_docker:-}" DISTRO="${distro}" \
-				make -e "${target_image}"
 			;;
 		"dracut")
 			sudo -E BUILD_METHOD="dracut" make -e "${target_image}"
