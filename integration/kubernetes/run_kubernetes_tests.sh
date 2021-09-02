e239552908c1a1efaa82f43a8f254af210e2da66
Jack Rieck <jack.rieck@sendgrid.com>
2021-09-02 11:37:38 -0400

    only run volume tests for now

--- integration/kubernetes/run_kubernetes_tests.sh
+++ integration/kubernetes/run_kubernetes_tests.sh
@@ -20,36 +20,36 @@ KATA_HYPERVISOR="${KATA_HYPERVISOR:-qemu}"
 trap '${kubernetes_dir}/cleanup_env.sh' EXIT
 
 K8S_TEST_UNION=("k8s-attach-handlers.bats" \
-	"k8s-block-volume.bats" \
-	"k8s-configmap.bats" \
-	"k8s-copy-file.bats" \
-	"k8s-cpu-ns.bats" \
-	"k8s-credentials-secrets.bats" \
-	"k8s-custom-dns.bats" \
-	"k8s-empty-dirs.bats" \
-	"k8s-env.bats" \
-	"k8s-exec.bats" \
-	"k8s-expose-ip.bats" \
-	"k8s-job.bats" \
-	"k8s-limit-range.bats" \
-	"k8s-liveness-probes.bats" \
-	"k8s-memory.bats" \
-	"k8s-number-cpus.bats" \
-	"k8s-oom.bats" \
-	"k8s-parallel.bats" \
-	"k8s-pid-ns.bats" \
-	"k8s-pod-quota.bats" \
-	"k8s-port-forward.bats" \
-	"k8s-projected-volume.bats" \
-	"k8s-qos-pods.bats" \
-	"k8s-replication.bats" \
-	"k8s-scale-nginx.bats" \
-	"k8s-sysctls.bats" \
-	"k8s-security-context.bats" \
-	"k8s-shared-volume.bats" \
-	"k8s-volume.bats" \
-	"k8s-nginx-connectivity.bats" \
-	"k8s-hugepages.bats")
+	"k8s-block-volume.bats")
+	#"k8s-configmap.bats" \
+	#"k8s-copy-file.bats" \
+	#"k8s-cpu-ns.bats" \
+	#"k8s-credentials-secrets.bats" \
+	#"k8s-custom-dns.bats" \
+	#"k8s-empty-dirs.bats" \
+	#"k8s-env.bats" \
+	#"k8s-exec.bats" \
+	#"k8s-expose-ip.bats" \
+	#"k8s-job.bats" \
+	#"k8s-limit-range.bats" \
+	#"k8s-liveness-probes.bats" \
+	#"k8s-memory.bats" \
+	#"k8s-number-cpus.bats" \
+	#"k8s-oom.bats" \
+	#"k8s-parallel.bats" \
+	#"k8s-pid-ns.bats" \
+	#"k8s-pod-quota.bats" \
+	#"k8s-port-forward.bats" \
+	#"k8s-projected-volume.bats" \
+	#"k8s-qos-pods.bats" \
+	#"k8s-replication.bats" \
+	#"k8s-scale-nginx.bats" \
+	#"k8s-sysctls.bats" \
+	#"k8s-security-context.bats" \
+	#"k8s-shared-volume.bats" \
+	#"k8s-volume.bats" \
+	#"k8s-nginx-connectivity.bats" \
+	#"k8s-hugepages.bats")
 
 # we may need to skip a few test cases when running on non-x86_64 arch
 if [ -f "${cidir}/${arch}/configuration_${arch}.yaml" ]; then
