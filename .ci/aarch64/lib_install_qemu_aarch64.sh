fe276b47ec17e391b1ac5e52dfaad04445bdc6fd
Jianyong Wu <jianyong.wu@arm.com>
2021-09-22 15:53:39 +0800

    qemu: using script to apply qemu patch
    remove this ugly way of applying patch in qemu build for arm64.
    
    Depends-on: github.com/kata-containers/kata-containers#2697
    Fixes: #3994
    Signed-off-by: Jianyong Wu <jianyong.wu@arm.com>

--- .ci/aarch64/lib_install_qemu_aarch64.sh
+++ .ci/aarch64/lib_install_qemu_aarch64.sh
@@ -27,11 +27,7 @@ build_and_install_qemu() {
         [ -d "ui/keycodemapdb" ] || git clone  https://github.com/qemu/keycodemapdb.git --depth 1 ui/keycodemapdb
 
         # Apply required patches
-        QEMU_PATCHES_PATH="${PACKAGING_DIR}/qemu/patches/5.1.x"
-        for patch in ${QEMU_PATCHES_PATH}/*.patch; do
-                echo "Applying patch: $patch"
-                patch -p1 <"$patch"
-        done
+	${PACKAGING_DIR}/scripts/patch_qemu.sh ${CURRENT_QEMU_VERSION} ${PACKAGING_DIR}/qemu/patches
 
         echo "Build Qemu"
         "${QEMU_CONFIG_SCRIPT}" "qemu" | xargs ./configure
