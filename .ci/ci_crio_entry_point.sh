f180432bc1c9414dd084e13d60aec272b77f505b
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2022-01-19 11:12:41 +0100

    ci: crio: Bump CRI-O latest release
    CRI-O latest release is now 1.23, and we should be testing against the
    same version of Kubernetes.
    
    Fixes: #4382
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- .ci/ci_crio_entry_point.sh
+++ .ci/ci_crio_entry_point.sh
@@ -36,7 +36,7 @@ export CI_JOB="EXTERNAL_CRIO"
 export INSTALL_KATA="yes"
 export GO111MODULE=auto
 
-latest_release="1.22"
+latest_release="1.23"
 
 sudo bash -c "cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
