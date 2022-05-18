c88a87c1b006f4aa321e16bfad3369effd666a86
Wainer dos Santos Moschetta <wainersm@redhat.com>
2022-05-18 19:19:00 -0300

    ci: make install_virtiofsd.sh to honor DESTDIR
    Changed the virtiofsd install script to take in consideration the DESTDIR variable
    (if exported) to install binary.
    
    Fixes #4798
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- .ci/install_virtiofsd.sh
+++ .ci/install_virtiofsd.sh
@@ -13,6 +13,8 @@ set -o errtrace
 cidir=$(dirname "$0")
 source "${cidir}/lib.sh"
 
+DESTDIR="${DESTDIR:-/}"
+
 main() {
 	local buildscript="${katacontainers_repo_dir}/tools/packaging/kata-deploy/local-build/kata-deploy-binaries.sh"
 
@@ -21,8 +23,14 @@ main() {
 
 	pushd $katacontainers_repo_dir
 	sudo -E PATH=$PATH bash ${buildscript} --build=virtiofsd
-	sudo tar -xvJpf build/kata-static-virtiofsd.tar.xz -C /
-	sudo ln -sf /opt/kata/libexec/virtiofsd /usr/libexec/virtiofsd
+	sudo tar -xvJpf build/kata-static-virtiofsd.tar.xz -C "${DESTDIR}"
+	# Kata CI requires the link but this isn't true to all scenarios,
+	# for example, on OpenShift CI everything should be installed under
+	# /opt/kata. So do not try to create the link unless the directory
+	# exist.
+	[ -d "${DESTDIR}/usr/libexec" ] && \
+		sudo ln -sf "${DESTDIR}/opt/kata/libexec/virtiofsd" \
+			"${DESTDIR}/usr/libexec/virtiofsd"
 	popd
 }
 
