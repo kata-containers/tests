3e45a882e0fc8678089b7edecd921abded8bcc3b
Wainer dos Santos Moschetta <wainersm@redhat.com>
2022-08-16 19:22:33 -0300

    ci/install_virtiofsd: call build_static_artifact_and_install()
    The .ci/instal_virtiofsd.sh is already using the kata-deploy's
    script, here it changed to call the common
    build_static_artifact_and_install().
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- .ci/install_virtiofsd.sh
+++ .ci/install_virtiofsd.sh
@@ -14,26 +14,21 @@ cidir=$(dirname "$0")
 source "${cidir}/lib.sh"
 
 DESTDIR="${DESTDIR:-/}"
+KATA_BUILD_CC="${KATA_BUILD_CC:-no}"
 
 main() {
 	bash "${cidir}/install_rust.sh" && source "$HOME/.cargo/env"
 
-	local buildscript="${katacontainers_repo_dir}/tools/packaging/kata-deploy/local-build/kata-deploy-binaries.sh"
+	build_static_artifact_and_install "virtiofsd"
 
-	# Just in case the kata-containers repo is not cloned yet.
-	clone_katacontainers_repo
-
-	pushd $katacontainers_repo_dir
-	sudo -E PATH=$PATH bash ${buildscript} --build=virtiofsd
-	sudo tar -xvJpf build/kata-static-virtiofsd.tar.xz -C "${DESTDIR}"
 	# Kata CI requires the link but this isn't true to all scenarios,
 	# for example, on OpenShift CI everything should be installed under
 	# /opt/kata. So do not try to create the link unless the directory
 	# exist.
-	[ -d "${DESTDIR}/usr/libexec" ] && \
+	if [ -d "${DESTDIR}/usr/libexec" -a "${KATA_BUILD_CC}" == "no" ]; then
 		sudo ln -sf "${DESTDIR}/opt/kata/libexec/virtiofsd" \
 			"${DESTDIR}/usr/libexec/virtiofsd"
-	popd
+	fi
 }
 
 main "$@"
