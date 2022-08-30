92546502e5542d68b67207636242b5eb89dde16e
Wainer dos Santos Moschetta <wainersm@redhat.com>
2022-08-30 13:19:44 -0300

    ci/install_qemu: install tdvf alongside QEMU
    Call the install_tdvf.sh script from install_qemu.sh in case QEMU type
    is TDX. Also refactor the install_tdvf.sh to use the
    build_static_artifact_and_install() method.
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- .ci/install_tdvf.sh
+++ .ci/install_tdvf.sh
@@ -14,16 +14,11 @@ cidir=$(dirname "$0")
 source "${cidir}/lib.sh"
 
 main() {
-	local buildscript="${katacontainers_repo_dir}/tools/packaging/kata-deploy/local-build/kata-deploy-binaries.sh"
+	build_static_artifact_and_install "tdx-tdvf"
 
-	# Just in case the kata-containers repo is not cloned yet.
-	clone_katacontainers_repo
-
-	pushd $katacontainers_repo_dir
-	sudo -E PATH=$PATH bash ${buildscript} --build=cc-tdx-tdvf
-	sudo tar -xvJpf build/kata-static-cc-tdx-tdvf.tar.xz -C /
-	sudo ln -sf /opt/confidential-containers/share/tdvf /usr/share/tdvf
-	popd
+        if [ "${KATA_BUILD_CC:-no}" == "yes" ]; then
+		sudo ln -sf /opt/confidential-containers/share/tdvf /usr/share/tdvf
+	fi
 }
 
 main "$@"
