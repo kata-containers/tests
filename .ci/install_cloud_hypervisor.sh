39ece9ab237b5be1fa4d6ba131cabf56ee5ed030
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2022-10-04 21:58:54 +0200

    ci/install_cloud_hypervisor: call build_static_artifact_and_install()
    The .ci/instal_cloud_hypervisor.sh is already using the kata-deploy's
    script, here it changed to call the common
    build_static_artifact_and_install().
    
    Fixes: #5537
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- .ci/install_cloud_hypervisor.sh
+++ .ci/install_cloud_hypervisor.sh
@@ -14,16 +14,7 @@ cidir=$(dirname "$0")
 source "${cidir}/lib.sh"
 
 main() {
-	local buildscript="${katacontainers_repo_dir}/tools/packaging/kata-deploy/local-build/kata-deploy-binaries.sh"
-
-	# Just in case the kata-containers repo is not cloned yet.
-	clone_katacontainers_repo
-
-	pushd $katacontainers_repo_dir
-	sudo -E PATH=$PATH bash ${buildscript} --build=cloud-hypervisor
-	sudo tar -xvJpf build/kata-static-cloud-hypervisor.tar.xz -C /
-	sudo ln -sf /opt/kata/bin/cloud-hypervisor /usr/bin/cloud-hypervisor
-	popd
+	build_static_artifact_and_install "cloud-hypervisor"
 }
 
 main "$@"
