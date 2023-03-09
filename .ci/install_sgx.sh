69ee6381a52a764c64c2c3b7be236b3f597beedb
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2023-03-09 19:15:59 +0000

    ci: Install kata+sgx components
    This PR installs kata+sgx components
    
    Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>

--- .ci/install_sgx.sh
+++ .ci/install_sgx.sh
@@ -22,7 +22,7 @@ sudo -E kubectl apply -f https://raw.githubusercontent.com/kata-containers/kata-
 sudo -E kubectl -n kube-system wait --timeout=10m --for=condition=Ready -l name=kata-deploy pod
 sudo -E kubectl apply -f https://raw.githubusercontent.com/kata-containers/kata-containers/main/tools/packaging/kata-deploy/runtimeclasses/kata-runtimeClasses.yaml
 sudo rm /opt/kata/share/defaults/kata-containers/configuration.toml
-sudo cp /opt/kata/share/defaults/kata-containers/configuration-clh.toml /opt/kata/share/defaults/kata-containers/configuration.toml
+sudo ln -sf /opt/kata/share/defaults/kata-containers/configuration-clh.toml /opt/kata/share/defaults/kata-containers/configuration.toml
 sudo systemctl daemon-reload
 sudo -E ./cleanup_env.sh
 popd
