a5b95e837dede8ee163789102900800ef8aaace9
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2023-03-13 21:53:52 +0100

    containerd: crio: Add ConfigPath to the tests config
    This is also needed in order to workaround the fact that runtime-rs
    still doesn't understand / translates $PREFIX passed to the Makefile to
    the rust code responsible for loading the config.
    
    Fixes: #5537
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- .ci/configure_containerd_for_kata.sh
+++ .ci/configure_containerd_for_kata.sh
@@ -9,6 +9,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+PREFIX="${PREFIX:-/opt/kata}"
 readonly runc_path=$(command -v runc)
 
 sudo mkdir -p /etc/containerd/
@@ -29,6 +30,8 @@ cat << EOF | sudo tee /etc/containerd/config.toml
         [plugins.cri.containerd.runtimes.kata]
            runtime_type = "io.containerd.kata.v2"
            privileged_without_host_devices = true
+           [plugins.cri.containerd.runtimes.kata.options]
+              ConfigPath = "${PREFIX}/share/defaults/kata-containers/configuration.toml"
     [plugins.cri.registry.mirrors."localhost:5000"]
       endpoint = ["http://localhost:5000"]
 EOF
