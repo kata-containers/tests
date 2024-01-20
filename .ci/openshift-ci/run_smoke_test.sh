ab9da6bf9fd7e609a0d1644cbe3f0737cbd3c995
Lukáš Doktor <ldoktor@redhat.com>
2024-01-20 14:10:25 +0100

    ci/openshift-ci: Use loop instead of curl --retry-all-errors
    The --retry-all-errors argument is not supported on the CI runners, use
    a busy loop instead.
    
    Fixes: #5802
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- .ci/openshift-ci/run_smoke_test.sh
+++ .ci/openshift-ci/run_smoke_test.sh
@@ -56,7 +56,8 @@ else
     port=$(oc get service/http-server-service -o jsonpath='{.spec.ports[0].nodePort}')
 fi
 
-curl "${host}:${port}${hello_file}" -s -o hello_msg.txt --retry 60 --retry-delay 1 --retry-all-errors
+info "Wait for the HTTP server to respond"
+waitForProcess 60 1 "curl '${host}:${port}${hello_file}' -s -o hello_msg.txt"
 
 grep "${hello_msg}" hello_msg.txt > /dev/null
 test_status=$?
